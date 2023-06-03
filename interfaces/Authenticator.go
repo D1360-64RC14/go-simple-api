b20779b423ad675cd75e4b4c99b4d7c4bd06342a
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-06-02 23:34:11 -0300

    Update signature at Authenticator interface

--- interfaces/Authenticator.go
+++ interfaces/Authenticator.go
@@ -2,5 +2,5 @@ package interfaces
 
 type Authenticator interface {
 	GenerateToken(id int, email string) (string, error)
-	IsTokenValid(inputToken string) (bool, error)
+	IsTokenValid(inputToken string) bool
 }
