cf42f34db659fc97b3ae01a3bd564b2c76398e8f
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-06-03 15:07:21 -0300

    Refact: turn `TestPathUserId` into test case slice

--- middlewares/validate/PathUserId_test.go
+++ middlewares/validate/PathUserId_test.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +14,27 @@ func init() {
 }
 
 func TestPathUserId(t *testing.T) {
+	testCases := []struct {
+		inputId  string
+		respCode int
+		respBody string
+	}{
+		{"42", http.StatusOK, "42"},
+		{"0", http.StatusOK, "0"},
+		{"-5", http.StatusOK, "-5"},
+		{"000", http.StatusOK, "0"},
+		{"99", http.StatusOK, "99"},
+		{"+100", http.StatusOK, "100"},
+		{"1337", http.StatusOK, "1337"},
+		{"foo", http.StatusBadRequest, "{\"error\":\"The user ID in the path should be an integer, not 'foo'\"}"},
+		{"bar", http.StatusBadRequest, "{\"error\":\"The user ID in the path should be an integer, not 'bar'\"}"},
+		{".", http.StatusBadRequest, "{\"error\":\"The user ID in the path should be an integer, not '.'\"}"},
+		{",", http.StatusBadRequest, "{\"error\":\"The user ID in the path should be an integer, not ','\"}"},
+		{":", http.StatusBadRequest, "{\"error\":\"The user ID in the path should be an integer, not ':'\"}"},
+		{":id", http.StatusBadRequest, "{\"error\":\"The user ID in the path should be an integer, not ':id'\"}"},
+		{"id", http.StatusBadRequest, "{\"error\":\"The user ID in the path should be an integer, not 'id'\"}"},
+	}
+
 	engine := gin.New()
 
 	engine.GET("/:id", PathUserId, func(ctx *gin.Context) {
@@ -23,56 +42,20 @@ func TestPathUserId(t *testing.T) {
 		ctx.String(http.StatusOK, fmt.Sprint(id))
 	})
 
-	t.Run("WithValidIdType", testPathUserId_WithValidIdType(engine))
-	t.Run("WithInvalidIdType", testPathUserId_WithInvalidIdType(engine))
-}
-
-func testPathUserId_WithValidIdType(engine *gin.Engine) func(*testing.T) {
-	return func(t *testing.T) {
-		validIds := []string{"42", "0", "-5", "000", "99", "+100", "1337"}
-
-		for _, userId := range validIds {
-			intId, _ := strconv.ParseInt(userId, 10, 32)
-
-			resultingCode := http.StatusOK
-			resultingBody := fmt.Sprint(intId)
-
+	for i, _case := range testCases {
+		t.Run(fmt.Sprintf("token_%d", i), func(t *testing.T) {
 			rec := httptest.NewRecorder()
-			req := httptest.NewRequest("GET", "/"+userId, nil)
+			req := httptest.NewRequest("GET", "/"+_case.inputId, nil)
 
 			engine.ServeHTTP(rec, req)
 			body := rec.Body.String()
 
-			if rec.Code != resultingCode {
-				t.Errorf("Code should be '%d', got '%d'", resultingCode, rec.Code)
+			if rec.Code != _case.respCode {
+				t.Errorf("Code should be '%d', got '%d'", _case.respCode, rec.Code)
 			}
-			if body != resultingBody {
-				t.Errorf("Returned id should be '%s', got '%s'", resultingBody, body)
+			if body != _case.respBody {
+				t.Errorf("Returned id should be '%s', got '%s'", _case.respBody, body)
 			}
-		}
-	}
-}
-
-func testPathUserId_WithInvalidIdType(engine *gin.Engine) func(*testing.T) {
-	return func(t *testing.T) {
-		invalidIds := []string{"foo", "bar", ".", ",", ":", ":id", "id"}
-
-		for _, userId := range invalidIds {
-			resultingCode := http.StatusBadRequest
-			resultingBody := fmt.Sprintf("The user ID in the path should be an integer, not '%s'", userId)
-
-			rec := httptest.NewRecorder()
-			req := httptest.NewRequest("GET", "/"+userId, nil)
-
-			engine.ServeHTTP(rec, req)
-			body := rec.Body.String()
-
-			if rec.Code != resultingCode {
-				t.Errorf("Code should be '%d', got '%d'", resultingCode, rec.Code)
-			}
-			if !strings.Contains(body, resultingBody) {
-				t.Errorf("Returned body should have '%s', got '%s'", resultingBody, body)
-			}
-		}
+		})
 	}
 }
